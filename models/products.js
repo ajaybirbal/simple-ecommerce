70348935162f419854a9bf6cd056ecd92dee8d84
ajaybirbal <ajaybirbal@gmail.com>
2019-11-02 11:46:19 +0530

    Admin Page coding begins

---
+++ models/products.js
@@ -0,0 +1,48 @@
+//Schema for user schema table
+
+
+const mongoose = require('mongoose');
+
+let userSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique:true
+    },
+    description:{
+        type:String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: String
+    },
+    shipping: {
+        timeNeeded: Number,
+        price: {
+            type: Number,
+            require: true
+        }
+    },
+    DateAdded: {
+        type: Date,
+        default: Date.now()
+    },
+    seller: {
+        Id: {
+            type: String
+        }
+    },
+    quantity: {
+        type: Number
+    },
+    tags: [String]
+})
+
+let Products = mongoose.model('Users', userSchema);
+
+
+exports.Products = Products;
